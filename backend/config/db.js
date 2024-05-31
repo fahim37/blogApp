5f2d069422691ad6ec3573b2655375e68c0ae77b
Fahim Ahmed Emon <63098250+fahim37@users.noreply.github.com>
2024-05-31 19:03:43 +0600

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const colors = require("colors");
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://fahim37:fahim0037@blogcluster.udrdown.mongodb.net/?retryWrites=true&w=majority&appName=BlogCluster');
+        await mongoose.connect('mongodb+srv://fahim37:fahim0037@blogcluster.udrdown.mongodb.net/blogapp?retryWrites=true&w=majority&appName=BlogCluster');
         console.log(
             `Connected to Mongodb Database ${mongoose.connection.host}`.bgMagenta
                 .white
